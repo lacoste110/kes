ca7b8faa61b47b6a61985a6dfa9ff8c918775592
Yaroslav <lacoste230790@gmail.com>
2020-08-31 18:23:15 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const answers = [];
+
+answers[0] = prompt('What is you neme?', '');
+answers[1] = prompt('What is you sourneme?', '');
+answers[2] = prompt('how oll are you?', '' );
+
+alert(`Hello, ${answers[0]}`);
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr, ' ', decr);
